425b8e12f282429743cae3c4d95195b0c22707de
Axcce1erat <axelschwarz@gmx.de>
2019-12-06 15:30:22 +0100

    refactor code

--- Thanos.java
+++ Thanos.java
@@ -5,7 +5,7 @@ public class Thanos {
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
-        ArrayList<Hero> heroes = new ArrayList(10);
+        ArrayList<Hero> heroes = new ArrayList<>();
 
         // TODO 2 : Add those heroes to the list
         // name: Black Widow, age: 34
@@ -16,15 +16,16 @@ public class Thanos {
         // name: Thor, age: 1500
         // name: Hulk, age: 49
         // name: Doctor Strange, age: 42
-        Hero widow = new Hero("Balck Widow", 34);
-        Hero captain = new Hero("Captain America", 100);
-        Hero vision = new Hero("Vision", 3);
-        Hero iron = new Hero("Iron Man", 48);
-        Hero witch = new Hero("Scarlet Witch", 29);
-        Hero thor = new Hero("Thor", 1500);
-        Hero hulk = new Hero("Hulk", 49);
-        Hero dr = new Hero("Doctor Strange", 42);
+        heroes.add(new Hero("Balck Widow", 34));
+        heroes.add(new Hero("Captain America", 100));
+        heroes.add(new Hero("Vision", 3));
+        heroes.add(new Hero("Iron Man", 48));
+        heroes.add(new Hero("Scarlet Witch", 29));
+        heroes.add(new Hero("Thor", 1500));
+        heroes.add(new Hero("Hulk", 49));
+        heroes.add(new Hero("Doctor Strange", 42));
 
+        /*
         heroes.add(widow);
         heroes.add(captain);
         heroes.add(vision);
@@ -33,15 +34,17 @@ public class Thanos {
         heroes.add(thor);
         heroes.add(hulk);
         heroes.add(dr);
+        */
 
         // TODO 3 : It's Thor birthday, now he's 1501
         heroes.get(5).setAge(1501);
+        //System.out.println(heroes);
         // TODO 4 : Shuffle the heroes list
         Collections.shuffle(heroes);
         // TODO 5 : Keep only the half of the list
-        List<Hero> snap = heroes.subList(4,8);
+        heroes.subList(0, heroes.size()/2).clear();
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
-        for(Hero after: snap){
+        for(Hero after: heroes){
             System.out.println(after.getName());
         }
     }
